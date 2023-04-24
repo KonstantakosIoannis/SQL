3d8e71e6721d921bfb993a0b219022f7aeaf1327
KonstantakosIoannis <128243262+KonstantakosIoannis@users.noreply.github.com>
2023-04-24 12:29:40 +0300

    Update 4.6.Compare Between Rows.sql

--- Data Analysis using SQL server/4.6.Compare Between Rows.sql
+++ Data Analysis using SQL server/4.6.Compare Between Rows.sql
@@ -10,23 +10,23 @@ INSERT INTO loans (id, price, months)
 VALUES (1, 100, 1),
        (1, 300, 2),
        (1, 200, 3),
-	   (3, 20, 8),
-	   (3, 2, 9),
-	   (3, 2000, 10),
-	   (3, 45, 11);
+       (3, 20, 8),
+       (3, 2, 9),
+       (3, 2000, 10),
+       (3, 45, 11);
 
 
 SELECT * FROM loans;
 
-SELECT * FROM loans as l1
-inner join loans as l2
-on l1.id=l2.id
+SELECT * FROM loans AS l1
+INNER JOIN loans AS l2
+ON l1.id=l2.id
 AND l1.months BETWEEN (l2.months-2) AND  (l2.months) 
-order by l2.months;
+ORDER BY l2.months;
 
-SELECT l2.id , l2.months , MAX(l1.price) as XYZ FROM loans as l1
-inner join loans as l2
-on l1.id=l2.id
+SELECT l2.id , l2.months , MAX(l1.price) AS XYZ FROM loans AS l1
+INNER JOIN loans AS l2
+ON l1.id=l2.id
 AND l1.months BETWEEN (l2.months-2) AND  (l2.months) 
 GROUP BY l2.id , l2.months ;
 
